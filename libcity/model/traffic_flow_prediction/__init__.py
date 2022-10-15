2610e2d584d71c4c390b1562c2a15acc35dd3307
SonghuaHu-UMD <hsonghua@UMD.EDU>
2022-10-15 12:47:15 -0400

    read_me_add

--- libcity/model/traffic_flow_prediction/__init__.py
+++ libcity/model/traffic_flow_prediction/__init__.py
@@ -1,13 +1,9 @@
 from libcity.model.traffic_flow_prediction.AGCRN import AGCRN
 from libcity.model.traffic_flow_prediction.ASTGCN import ASTGCN
-from libcity.model.traffic_flow_prediction.STSGCN import STSGCN
-from libcity.model.traffic_flow_prediction.MultiATGCN3UT import MultiATGCN3UT
 from libcity.model.traffic_flow_prediction.MultiATGCN import MultiATGCN
 
 __all__ = [
     "AGCRN",
     "ASTGCN",
-    "STSGCN",
     "MultiATGCN",
-    "MultiATGCN3UT"
 ]
--- libcity/model/traffic_speed_prediction/__init__.py
+++ libcity/model/traffic_speed_prediction/__init__.py
@@ -4,9 +4,7 @@ from libcity.model.traffic_speed_prediction.GWNET import GWNET
 from libcity.model.traffic_speed_prediction.MTGNN import MTGNN
 from libcity.model.traffic_speed_prediction.TGCN import TGCN
 from libcity.model.traffic_speed_prediction.RNN import RNN
-from libcity.model.traffic_speed_prediction.Seq2Seq import Seq2Seq
 from libcity.model.traffic_speed_prediction.GMAN import GMAN
-from libcity.model.traffic_speed_prediction.STTN import STTN
 from libcity.model.traffic_speed_prediction.FNN import FNN
 
 __all__ = [
@@ -15,8 +13,6 @@ __all__ = [
     "GWNET",
     "TGCN",
     "RNN",
-    "Seq2Seq",
     "GMAN",
-    "STTN",
     "FNN",
 ]
